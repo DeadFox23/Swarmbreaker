598c07ec092a2843546a49ab70e8ed252102c124
DeadFox23 <127043257+DeadFox23@users.noreply.github.com>
2024-11-07 10:44:10 +0100

    new file:   Swarmbreaker/Cs_Files/EntityEnemy.cs 	new file:   Swarmbreaker/Cs_Files/EntityPlayerCharacter.cs 	new file:   Swarmbreaker/Cs_Files/IEntity.cs 	new file:   Swarmbreaker/Pages/DefaultMap.cshtml 	new file:   Swarmbreaker/Pages/DefaultMap.cshtml.cs

---
+++ Swarmbreaker/Cs_Files/EntityPlayerCharacter.cs
@@ -0,0 +1,21 @@
+namespace Swarmbreaker.Cs_Files{
+public class EntityPlayerCharacter : IEntity
+{
+
+    public int Id {get;set;}
+    public int y {get;set;}
+    public int x {get;set;}
+    public double statBaseHP {get;set;}
+    public double statBaseAttack {get;set;}
+    public double statBonusAttack {get;set;}
+    public double statBonusArmor {get;set;}
+
+    public static int statXP;
+    public static int statLevel;
+    public double statAttackSpeed;
+
+    public void move(){}
+    public void death(){}
+    public void attack(){}
+    public void levelUp(){}
+}}
---
+++ Swarmbreaker/Pages/DefaultMap.cshtml.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Swarmbreaker.Pages{
+    public class DefaultMap: PageModel{
+
+    }
+}
