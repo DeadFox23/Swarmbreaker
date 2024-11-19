95c754f59ed4caf6463b9e9080892be4f445935d
DeadFox23 <127043257+DeadFox23@users.noreply.github.com>
2024-11-19 11:29:36 +0100

    starting enemy spawn

--- Swarmbreaker/Pages/DefaultMap.cshtml.cs
+++ Swarmbreaker/Pages/DefaultMap.cshtml.cs
@@ -1,9 +1,23 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Swarmbreaker.Cs_Files;
 
 namespace Swarmbreaker.Pages
 {
     public class DefaultMap : PageModel
     {
-        
+        [BindProperty]
+        public List<EntityEnemy> entities { get; set; }
+
+
+
+		public void OnGet() {
+            entities = new List<EntityEnemy>();
+            for (int i = 0; i < 10; i++) {
+				entities.Add(new EntityEnemy());
+			}
+            
+        }
     }
 }
