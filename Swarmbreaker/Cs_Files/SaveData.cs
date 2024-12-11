537d5b43d9008c446c8fcbf014cfc3dad000a8f6
Muemmelfrau <layer.nina@web.de>
2024-12-11 13:45:22 +0100

    Update SaveData.cs

--- Swarmbreaker/Cs_Files/SaveData.cs
+++ Swarmbreaker/Cs_Files/SaveData.cs
@@ -5,9 +5,9 @@
 		public static List<EntityEnemy> enemies { get; set; } = new List<EntityEnemy>();
 		public static List<EntityPlayerCharacter> players { get; set; } = new List<EntityPlayerCharacter>();
 	
-		public static void addEnemy(int y, int x, float speed, float statBaseHP, float statBaseAttack, float statBonusAttack, float statBonusArmor, int xpDrop, Boolean isBoss)
+		public static void addEnemy(int y, int x, float speed, float statBaseHP, float statBaseAttack, float statBonusAttack, float statBonusArmor, int xpDrop, Boolean isBoss, int enemyID)
 		{
-			enemies.Add(new EntityEnemy(y, x, speed, statBaseHP, statBaseAttack, statBonusAttack, statBonusArmor, xpDrop, isBoss, 1));
+			enemies.Add(new EntityEnemy(y, x, speed, statBaseHP, statBaseAttack, statBonusAttack, statBonusArmor, xpDrop, isBoss, enemyID));
 		}
 		public static void addPlayer(int y, int x, float speed, float statBaseHP, Weapon equippedWeapon, float statBonusAttack, float statBonusArmor, float statAttackSpeed)
 		{
